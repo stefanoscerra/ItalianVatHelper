d706b93f9c11a68e9d46e777570b541c4c78c894
stefanoscerra <stefano.scerra@gmail.com>
2017-05-04 14:44:42 +0200

    initial commit

---
+++ ItalianVatHelper/ItalianVatHelper/ItalianVatHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ItalianVatHelper
+{
+    public class ItalianVatHelper
+    {
+        public static int CalcCheckDigit(string str)
+        {
+            if (!Regex.IsMatch(str, @"\d{10}"))
+            {
+                throw new Exception("A 10 digits string is required");
+            }
+
+            var firstFiveOddIndex = str.Where((ch, i) => i % 2 == 0).Select(ch => int.Parse(ch.ToString()));
+            int x = firstFiveOddIndex.Sum();
+
+            var evenIndexDigits = str.Where((ch, i) => i % 2 != 0).Select(ch => int.Parse(ch.ToString()));
+            int y = evenIndexDigits.Sum(digit =>
+            {
+                var result = 2 * digit;
+                if (result > 9)
+                {
+                    result -= 9;
+                }
+                return result;
+            });
+
+            int t = (x + y) % 10;
+            int c = (10 - t) % 10;
+
+            return c;
+        }
+
+        public static bool VerifyCheckDigit(string vatNumber)
+        {
+            if (!Regex.IsMatch(vatNumber, @"\d{11}"))
+            {
+                throw new Exception("An 11 digits VAT number is required");
+            }
+
+            var oddIndexDigits = vatNumber.Where((ch, i) => i % 2 == 0 && i < vatNumber.Length - 1).Select(ch => int.Parse(ch.ToString()));
+            int x = oddIndexDigits.Sum();
+            var evenIndexDigits = vatNumber.Where((ch, i) => i % 2 != 0).Select(ch => int.Parse(ch.ToString()));
+            int y = evenIndexDigits.Sum(d => 2 * d);
+            int z = evenIndexDigits.Where(d => d >= 5).Count();
+            int t = (x + y + z) % 10;
+            int c = (10 - t) % 10;
+
+            int checkDigit = int.Parse(vatNumber.Last().ToString());
+            return checkDigit == c;
+        }
+    }
+}
---
+++ ItalianVatHelper/ItalianVatHelper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItalianVatHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Italian VAT validator & generator\n");
+
+            long MAX = (long)Math.Pow(10, 10);
+            Random r = new Random();
+            long random = (long)(MAX * r.NextDouble());
+            string format = new string('0', 10);
+            string paddedRandom = random.ToString(format);
+
+            int checkDigit = ItalianVatHelper.CalcCheckDigit(paddedRandom);
+            string vat = paddedRandom + checkDigit;
+
+            Console.WriteLine("Partita IVA: " + vat);
+            bool valid = ItalianVatHelper.VerifyCheckDigit(vat);
+            Console.WriteLine("Validity check: " + (valid ? "OK" : "Not valid"));
+
+            Console.WriteLine("\nPress a key to exit...");
+            Console.ReadKey();
+        }
+        
+    }
+}
